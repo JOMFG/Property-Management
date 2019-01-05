228f822049b0d204d771172d87144b8af8cea5bd
Daven <davencasia@gmail.com>
2019-01-05 12:21:37 -0600

    Added validators for property

---
+++ src/controller/property.ts
@@ -0,0 +1,12 @@
+import { Response, Request, NextFunction } from "express";
+import { validationResult } from "express-validator/check";
+
+export function postProperty(req: Request, res: Response, next: NextFunction) {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  return res.json({});
+}
--- src/models/property.ts
+++ src/models/property.ts
@@ -1,4 +1,5 @@
 import { db } from "../config/db";
+import { check, validationResult } from 'express-validator/check';
 
 export type Property = {
   id: string;
@@ -7,6 +8,16 @@ export type Property = {
   address: string;
   description: string;
 };
+
+
+export const validator = [
+  check('id').isString(),
+  check('price').isNumeric(),
+  check('city').isString(),
+  check('address').isString(),
+  check('description').isString(),
+]
+
 
 export function saveProperty(property: Property) {
   return new Promise((resolve, reject) => {
